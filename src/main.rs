395430ed481225e71fe509de6c903a47a9338871
fireXtract <1490507+fireXtract@users.noreply.github.com>
2024-12-02 01:39:18 -0600

    it was working here

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,28 @@ fn main() {
 
     let mut safe_count = 0;
     'outer: while let Some(Ok(report)) = reports.next() {
-        // let report_nums = report.split_whitespace().into_iter().map(|num| num.parse::<i32>().unwrap());
-        println!("line");
+        let report_nums = report.split_whitespace()
+            .map(|num| num.parse::<i32>().unwrap())
+            .collect::<Vec<_>>();
+        // println!("line");
         let mut maybe_asc = false;
-        for (i, level) in report.split_whitespace().into_iter().enumerate().skip(1) {
-            if let Some(prev_level) = report.split_whitespace().nth(i - 1) {
-                if i <= 1 {
-                    maybe_asc = prev_level < level;
-                }
-                println!("prev: {} cur: {} maybe_asc: {} gt: {}", prev_level, level, maybe_asc, prev_level >= level);
-                if maybe_asc && prev_level > level {
-                    println!("first ");
-                    continue 'outer;
-                } else if !maybe_asc && prev_level < level {
-                    println!("second ");
-                    continue 'outer;
-                }
-            };
+        let mut first_run = true;
+        for window in report_nums.windows(2) {
+            let level = window[0];
+            let next_level = window[1];
+            if first_run {
+                first_run = false;
+                maybe_asc = level < next_level;
+            }
+            // println!("prev: {} cur: {} maybe_asc: {} gt: {}", level, next_level, maybe_asc, level >= next_level);
+            if level.abs_diff(next_level) > 3 ||
+                (maybe_asc && level >= next_level) ||
+                (!maybe_asc && level <= next_level) {
+                println!("unsafe");
+                continue 'outer;
+            }
         }
+        println!("safe");
         safe_count += 1;
     }
 
