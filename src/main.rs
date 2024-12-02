0c5a3fe8aa7f114abc818b1471577845b49cc8a5
fireXtract <1490507+fireXtract@users.noreply.github.com>
2024-12-02 02:27:49 -0600

    further cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,42 +4,27 @@ use std::io::BufRead;
 fn main() {
     let stdin = io::stdin();
     let mut reports = stdin.lock().lines();
-
     let mut safe_count = 0;
-    'outer: while let Some(Ok(report)) = reports.next() {
-        let report_nums = report.split_whitespace()
+    while let Some(Ok(raw_report)) = reports.next() {
+        let parsed_report = raw_report.split_whitespace()
             .map(|num| num.parse::<i32>().unwrap())
             .collect::<Vec<_>>();
-        println!("line");
-        'middle: for i in -1..report_nums.len() as i32 {
-            let mut c_report_nums = report_nums.clone();
-            if i > -1 {
-                c_report_nums.remove(i as usize);
-            }
-
-            let mut maybe_asc = false;
-            let mut first_run = true;
-            for window in c_report_nums.windows(2) {
-                let level = window[0];
-                let next_level = window[1];
-                if first_run {
-                    first_run = false;
-                    maybe_asc = level < next_level;
-                }
-                println!("prev: {} cur: {} maybe_asc: {} gt: {}", level, next_level, maybe_asc, level >= next_level);
+        'middle: for i in 0..parsed_report.len() {
+            let mut report = parsed_report.clone();
+            report.remove(i);
+            let is_asc = report[0] < report[1];
+            for level_window in report.windows(2) {
+                let (level, next_level) = (level_window[0], level_window[1]);
+                // println!("prev: {} cur: {} maybe_asc: {} gt: {}", level, next_level, maybe_asc, level >= next_level);
                 if level.abs_diff(next_level) > 3 ||
-                    (maybe_asc && level >= next_level) ||
-                    (!maybe_asc && level <= next_level) {
-                    println!("unsafe");
+                    (is_asc && (level >= next_level)) ||
+                    (!is_asc && (level <= next_level)) {
                     continue 'middle;
                 }
             }
-
-            println!("safe");
             safe_count += 1;
             break;
         }
     }
-
     println!("safe_count: {}", safe_count);
 }
