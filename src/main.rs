5603c28537cc9162fe6e36f6a4d7f097cf5b94d3
fireXtract <1490507+fireXtract@users.noreply.github.com>
2024-12-03 01:30:12 -0600

    My solutions are getting worse

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,62 @@
+use regex::Regex;
 use std::io;
-use std::io::BufRead;
+use std::io::Read;
 
 fn main() {
     let stdin = io::stdin();
-    let mut reports = stdin.lock().lines();
-    let mut safe_count = 0;
-    while let Some(Ok(raw_report)) = reports.next() {
-        let parsed_report = raw_report.split_whitespace()
-            .map(|num| num.parse::<i32>().unwrap())
-            .collect::<Vec<_>>();
-        'middle: for i in 0..parsed_report.len() {
-            let mut report = parsed_report.clone();
-            report.remove(i);
-            let is_asc = report[0] < report[1];
-            for (&level, &next_level) in report.iter().zip(report.iter().skip(1)) {
-                if level.abs_diff(next_level) > 3 ||
-                    (is_asc && (level >= next_level)) ||
-                    (!is_asc && (level <= next_level)) {
-                    continue 'middle;
-                }
-            }
-            safe_count += 1;
-            break;
+    let mut input = String::new();
+    stdin.lock().read_to_string(&mut input).expect("Didn't get input");
+    let program = input.replace('\n', "");
+    let r_mul = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let r_do = Regex::new(r"do\(\)").unwrap();
+    let r_dont = Regex::new(r"don't\(\)").unwrap();
+    let mut sum = 0;
+
+    let mut indicies_do: Vec<usize> = r_do.find_iter(&*program).map(|m| m.start()).collect();
+    let mut indicies_dont: Vec<usize> = r_dont.find_iter(&*program).map(|m| m.start()).collect();
+    let mut mask: Vec<bool> = Vec::new();
+    let mut do_iter = indicies_do.iter().peekable();
+    let mut dont_iter = indicies_dont.iter().peekable();
+    let mut last_do_index = *do_iter.next().unwrap_or(&0);
+    let mut last_dont_index = *dont_iter.next().unwrap_or(&program.len());
+    let mut set_to = true;
+    for i in 0..program.len() {
+        mask.push(set_to);
+        if i == last_do_index {
+            set_to = true;
+            last_do_index = *do_iter.next().unwrap_or(&program.len());
+        }
+        if i == last_dont_index {
+            set_to = false;
+            last_dont_index = *dont_iter.next().unwrap_or(&program.len());
         }
     }
-    println!("safe_count: {}", safe_count);
+
+    for i in 0..program.len() {
+        if mask[i] == false {
+            print!("0");
+        } else {
+            print!("1");
+        }
+    }
+    println!();
+    println!("{}", &*program);
+    let mul_cm = r_mul.captures_iter(&*program);
+    for mul_caps in mul_cm {
+        let a: Vec<_> = mul_caps.iter().map(|m| m.unwrap().as_str()).collect();
+        println!("{}", a.join("|"));
+        for pair in mul_caps.iter().skip(1).zip(mul_caps.iter().skip(2)) {
+            if let (Some(left), Some(right)) = pair {
+                if mask[right.start()] {
+                    let left_num = left.as_str().parse::<i32>().unwrap();
+                    let right_num = right.as_str().parse::<i32>().unwrap();
+                    sum += left_num * right_num;
+                }
+            }
+        }
+    }
+
+    println!("sum: {}", sum);
 }
+
+
