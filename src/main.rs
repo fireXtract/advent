13ab77ab76b145449bfda2fc8d75a800c31c19c3
fireXtract <1490507+fireXtract@users.noreply.github.com>
2024-12-06 02:22:00 -0600

    super optimized?

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,23 @@ fn main() {
     let mut fixed_invalid_middle_sum = 0;
     let mut before: HashMap<i32, Vec<i32>> = HashMap::new();
     while let Some(Ok(puzzle_line)) = puzzle_lines.next() {
-        let rule: Vec<i32> = puzzle_line
-            .split('|')
-            .flat_map(|r| r.parse::<i32>().ok())
-            .collect();
+        let rule: Vec<i32> = puzzle_line.split('|')
+            .flat_map(|r| r.parse::<i32>().ok()).collect();
         if rule.len() > 0 {
             before.entry(rule[0]).or_insert(Vec::new()).push(rule[1]);
         } else {
             let mut page_nums: Vec<i32> = puzzle_line.split(',').flat_map(|r| r.parse::<i32>().ok()).collect();
             let valid: bool = page_nums.iter()
-                .all(|page_num| {
-                    let page_num_before = before.get(page_num);
-                    let mut p = page_nums.split(|x| *x == *page_num);
-                    let left = p.next().unwrap();
-                    !left.iter().any(|l| page_num_before.iter().any(|&b| (*b).contains(l)))
-                });
+                .all(|page_num|
+                    before.get(page_num)
+                        .map_or(true, |before_vec|
+                            !page_nums.iter().take_while(|&x| *x != *page_num)
+                                .any(|left_page| before_vec.iter().any(|&b| b == *left_page)),
+                        )
+                );
             if !valid {
-                page_nums.sort_by(|&a, &b| {
-                    let a_before = before.get(&a).unwrap();
-                    // Check if `b` is in `before[a]` (i.e., a should come before b)
-                    if a_before.contains(&b) {
+                page_nums.sort_by(|a, b| {
+                    if before.get(a).map(|a_before| a_before.contains(b)).unwrap_or_default() {
                         return Ordering::Less;
                     }
                     Ordering::Equal
@@ -39,7 +36,6 @@ fn main() {
             }
         }
     }
-
     println!("middle_sum: {}", middle_sum);
     println!("fixed_invalid_middle_sum: {}", fixed_invalid_middle_sum);
 }
