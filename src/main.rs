11ced6731df224200d901076b92a30b0b7decfbc
fireXtract <1490507+fireXtract@users.noreply.github.com>
2024-12-11 01:21:20 -0600

    recursive solution fast; cheated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io;
 use std::io::BufRead;
 
@@ -9,32 +10,34 @@ fn main() {
     while let Some(Ok(puzzle_line)) = puzzle_lines.next() {
         stones = puzzle_line.split(' ').map(|s| s.parse::<u64>().unwrap()).collect()
     }
-    for s in stones.clone() {
-        print!("{s} ")
+    let mut length = 0;
+    let mut cache: HashMap<(u64, u8), u64> = HashMap::new();
+    for stone in stones {
+        length += recurse_blink(stone, 0, &mut cache);
     }
-
-    for blink in 0..BLINKS {
-        let mut i = 0usize;
-        println!("iteration {blink}");
-        while i < stones.len() {
-            let s = stones[i];
-            match s {
-                _ if s == 0 => stones[i] = 1,
-                v if count_digits(v) % 2 == 0 => {
-                    let split_digits = split_number(v);
-                    stones[i] = split_digits.0;
-                    stones.insert(i + 1, split_digits.1);
-                    i += 1;
-                }
-                _ => stones[i] = s * 2024
-            }
-            i += 1;
-        }
-    }
-
-    println!("{:?}\nlen:{}", stones, stones.len());
+    println!("len:{}", length);
 }
 
+fn recurse_blink(stone: u64, depth: u8, cache: &mut HashMap<(u64, u8), u64>) -> u64 {
+    let mut has_key = false;
+    let result = match (stone, depth) {
+        _ if depth == BLINKS => 1,
+        _ if cache.contains_key(&(stone, depth)) => {
+            has_key = true;
+            cache[&(stone, depth)]
+        }
+        _ if stone == 0 => recurse_blink(1, depth + 1, cache),
+        (v, _) if count_digits(v) % 2 == 0 => {
+            let split_digits = split_number(v);
+            recurse_blink(split_digits.0, depth + 1, cache) + recurse_blink(split_digits.1, depth + 1, cache)
+        }
+        _ => recurse_blink(stone * 2024, depth + 1, cache)
+    };
+    if !has_key { cache.insert((stone, depth), result); };
+    result
+}
+
+#[inline(always)]
 fn count_digits(i: u64) -> u64 {
     let mut count = 1;
     let mut n = i;
@@ -44,11 +47,11 @@ fn count_digits(i: u64) -> u64 {
     }
     count
 }
+#[inline(always)]
 fn split_number(i: u64) -> (u64, u64) {
     let digit_count = count_digits(i);
     let half_digits = digit_count / 2;
 
-    // Calculate the divisor to separate the halves
     let divisor = 10u64.pow(half_digits as u32);
 
     let left_half = i / divisor;
