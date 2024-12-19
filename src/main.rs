5b96ead61d216e5daf96b0549dacbf4db77a5236
fireXtract <1490507+fireXtract@users.noreply.github.com>
2024-12-19 02:17:54 -0600

    reduce line count

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-use std::cmp::max;
 use std::collections::HashMap;
 use std::io;
 use std::io::BufRead;
 
 fn ways_possible(design: &str, patterns: &HashMap<u8, Vec<String>>, cache: &mut HashMap<String, usize>) -> usize {
-    if design.len() == 0 {
-        return 1;
-    }
-    if let Some(&result) = cache.get(design) {
-        return result;
-    }
+    if design.len() == 0 { return 1; }
+    if let Some(&result) = cache.get(design) { return result; }
     let mut ways = 0;
     for len in (1..=design.len()).rev() {
         let sub = &design[0..len];
@@ -32,13 +27,11 @@ fn main() {
     let mut puzzle_lines = io::stdin().lock().lines();
     let mut patterns: HashMap<u8, Vec<String>> = HashMap::new();
     let mut total_ways = 0usize;
-    let mut longest = 1usize;
     let mut cache: HashMap<String, usize> = HashMap::new();
     while let Some(Ok(puzzle_line)) = puzzle_lines.next() {
         let p_split: Vec<String> = puzzle_line.split(',').map(|s| s.trim().to_string()).collect();
         if p_split.len() > 1 {
             for p in p_split {
-                longest = max(longest, p.len());
                 let key = p.as_bytes().first().copied().unwrap();
                 patterns.entry(key).and_modify(|m| m.push(p.clone())).or_insert_with(|| vec![p]);
             }
