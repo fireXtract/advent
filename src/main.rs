60724245c152e41386481127e4060bc1ef72484a
fireXtract <1490507+fireXtract@users.noreply.github.com>
2024-12-24 02:18:57 -0600

    Part 1 finished

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,45 @@
-use std::collections::{HashMap, LinkedList};
+use std::collections::{BTreeSet, HashMap, HashSet};
 use std::io;
 use std::io::BufRead;
 
+type Graph = HashMap<String, HashSet<String>>;
 
 fn main() {
     let mut puzzle_lines = io::stdin().lock().lines();
-    let mut indices: HashMap<String, Vec<usize>> = HashMap::new();
-    let mut links: Vec<LinkedList<String>> = vec![];
-    let mut i = 0;
+    let mut graph: Graph = HashMap::new();
     while let Some(Ok(puzzle_line)) = puzzle_lines.next() {
         println!("{puzzle_line}");
         let mut split = puzzle_line.split('-');
         let left = split.next().unwrap().to_string();
         let right = split.next().unwrap().to_string();
-        let seen_left = indices.contains_key(&left);
-        let seen_right = indices.contains_key(&right);
-        match (seen_left, seen_right) {
-            (false, false) => {
-                indices.insert(left.clone(), vec![i]);
-                indices.insert(right.clone(), vec![i]);
-                links.push(LinkedList::from([left, right]));
-                i += 1;
-            }
-            (true, false) => {
-                let mut right_indices = vec![];
-                for &index in &indices[&left] {
-                    links[index]..push_back(right.clone());
-                    links[index].push_back(right.clone());
-                    right_indices.push(index);
-                }
-                indices.insert(right.clone(), right_indices);
-            }
-            (false, true) => {
-                let mut left_indices = vec![];
-                for &index in &indices[&right] {
-                    links[index].push_back(left.clone());
-                    left_indices.push(index);
-                }
-                indices.insert(left.clone(), left_indices);
-            }
-            (true, true) => {
-                for &index in &indices[&left] {
-                    links[index].push_back(right.clone());
-                }
-                for &index in &indices[&right] {
-                    links[index].push_back(left.clone());
+
+        add_edge(&mut graph, left.clone(), right.clone());
+        add_edge(&mut graph, right, left);
+        println!("{graph:?}");
+    }
+
+    let mut threes: HashSet<BTreeSet<String>> = HashSet::new();
+    for (c0, n0) in &graph {
+        for c1 in n0 {
+            for c2 in &graph[c1] {
+                //if graph[c2].contains(c1) && graph[c2].contains(c0) && graph[c0].contains(c2) && graph[c0].contains(c1) && graph[c1].contains(c0) && graph[c1].contains(c2) {
+                if graph[c2].contains(c1) && graph[c2].contains(c0) && graph[c0].contains(c2) {
+                    if c0.starts_with('t') || c1.starts_with('t') || c2.starts_with('t') {
+                        println!("{c0},{c1},{c2}");
+                        threes.insert(BTreeSet::from([c0.clone(), c1.clone(), c2.clone()]));
+                    }
                 }
             }
         }
-        println!("{indices:?}");
-        println!("{links:?}");
     }
+    println!();
+    println!("unique: {}", threes.len());
+    println!("{threes:?}");
 
     println!("EOL");
+}
+
+fn add_edge(graph: &mut Graph, node1: String, node2: String) {
+    graph.entry(node1.clone()).or_insert_with(HashSet::new).insert(node2.clone());
+    graph.entry(node2.clone()).or_insert_with(HashSet::new).insert(node1.clone());
 }
